1a059da9be84dc81ab0e386a509617828e50fcb9
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-03 20:39:17 +0530

    add new component

---
+++ components/JavaScript/button01.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const botton01 = () => {
+  return (
+    <div>botton01</div>
+  )
+}
+
+export default botton01
---
+++ components/TypeScript/button01.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const button01 = () => {
+  return (
+    <div>button01</div>
+  )
+}
+
+export default button01
