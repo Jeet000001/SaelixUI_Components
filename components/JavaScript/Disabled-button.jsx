059e2f93e5bbe5a28869b34eb00c706cbccda083
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-04 19:03:24 +0530

    add diabled button

---
+++ components/JavaScript/Disabled-button.jsx
@@ -0,0 +1,32 @@
+const Disabled_Button = ({
+  children,
+  className = "",
+  disabled = true,
+  ...props
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      {...props}
+      className={`
+        px-5 py-2 rounded-xl border-b-2 transition duration-300 ease-in-out
+        bg-black text-white border-gray-600
+        hover:bg-gray-900 active:scale-95
+        dark:bg-white dark:text-black dark:border-gray-400
+        dark:hover:bg-gray-100
+        disabled:bg-gray-400
+        disabled:text-gray-700
+        disabled:border-gray-300
+        disabled:cursor-not-allowed
+        disabled:opacity-60
+        disabled:hover:bg-gray-400
+        disabled:active:scale-100
+        ${className}
+      `}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Disabled_Button;
