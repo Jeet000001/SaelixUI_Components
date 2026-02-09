37464965a00bf0d3d9160c11cc4fe73e501e6d21
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-09 18:58:25 +0530

    Add avatar status component

---
+++ components/JavaScript/Avatar-status.jsx
@@ -0,0 +1,20 @@
+const Avatar_Status = ({ src, size = 40, isOnline = true }) => {
+  return (
+    <div
+      style={{ width: size, height: size }}
+      className="relative inline-block"
+    >
+      <img
+        src={src}
+        alt="avatar"
+        className="w-full h-full object-cover rounded-full"
+      />
+
+      {isOnline && (
+        <span className="absolute -bottom-0.5 right-1 w-3 h-3 bg-green-500 border-2 border-white rounded-full"></span>
+      )}
+    </div>
+  );
+};
+
+export default Avatar_Status;
