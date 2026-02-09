811a08d32070155f929cebf209bee4f15a19dfe3
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-09 19:28:13 +0530

    Add avatar character component

---
+++ components/JavaScript/Avatar-character.jsx
@@ -0,0 +1,17 @@
+const Avatar_character = ({
+  size = 40,
+  char,
+  bg = "bg-blue-300",
+  textColor = "text-blue-700",
+}) => {
+  return (
+    <div
+      style={{ width: size, height: size }}
+      className={`${bg} rounded-full flex justify-center items-center`}
+    >
+      <p className={`${textColor} font-bold`}>{char}</p>
+    </div>
+  );
+};
+
+export default Avatar_character;
