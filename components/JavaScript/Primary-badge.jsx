d2f2790cd7340f610d6ab4819f59f348080cd130
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-10 17:55:48 +0530

    Add primary badge component

---
+++ components/JavaScript/Primary-badge.jsx
@@ -0,0 +1,11 @@
+const Primary_Badge = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`inline-block bg-black hover:bg-gray-900 dark:bg-white dark:hover:bg-gray-100 text-white dark:text-black px-3 py-0.5 font-medium text-sm rounded-full ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Primary_Badge;
