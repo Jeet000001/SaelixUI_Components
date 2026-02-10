579b9257f8fa7886df13fd261bf964069b2b70d1
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-10 19:14:38 +0530

    Add destractive badge

---
+++ components/JavaScript/Destructive-badge.jsx
@@ -0,0 +1,11 @@
+const Destructive_Badge = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`inline-block bg-red-600 hover:bg-red-500 dark:bg-red-500 dark:hover:bg-red-400 text-white font-medium text-sm px-3 py-0.5 rounded-full ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Destructive_Badge;
