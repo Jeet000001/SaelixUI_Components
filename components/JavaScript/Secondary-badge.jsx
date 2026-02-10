90a264725719c9c90ae5f716f26a0f8e4d02ad0d
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-10 18:35:01 +0530

    add secondary badge component

---
+++ components/JavaScript/Secondary-badge.jsx
@@ -0,0 +1,11 @@
+const Secondary_Badge = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`inline-block bg-gray-100 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600  font-medium text-sm px-3 py-0.5 rounded-full ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Secondary_Badge;
