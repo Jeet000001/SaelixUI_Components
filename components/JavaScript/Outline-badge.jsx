17f4d3754245e17713206314886c9a44f6b9e941
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-10 19:31:48 +0530

    Add outline badge component

---
+++ components/JavaScript/Outline-badge.jsx
@@ -0,0 +1,11 @@
+const Outline_Badge = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`inline-block border border-gray-300 hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-950 dark:text-white px-3 py-0.5 font-medium text-sm rounded-full ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Outline_Badge;
