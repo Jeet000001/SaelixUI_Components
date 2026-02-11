6f86a90e44beef08b3fc3e977e19a2c685c0d9d9
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-11 18:13:34 +0530

    Add gradient Badge component

---
+++ components/JavaScript/Gradient-badge.jsx
@@ -0,0 +1,19 @@
+const Gradient_Badge = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`inline-block 
+      bg-linear-to-r from-blue-400 via-blue-500 to-indigo-500 
+      hover:from-blue-500 hover:via-blue-600 hover:to-indigo-600
+      text-white 
+      px-3 py-0.5 
+      font-medium text-sm 
+      rounded-full
+      transition-all duration-300
+      ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Gradient_Badge;
